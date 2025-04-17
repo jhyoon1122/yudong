2a09778b70ec6f4ec86ccbb56f8ebc2405d97efb
jhyoon1122 <goose1234@naver.com>
2025-04-17 21:08:50 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Seoul Living Population Proxy API</h1>
+      <p>Use <code>/api/seoul?start=9&end=12&date=20230401</code> to fetch data.</p>
+    </div>
+  );
+}
